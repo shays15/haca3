c6515477fec87dff1be75f4ccd0f12b6d69e9b3c
shays15 <122406050+shays15@users.noreply.github.com>
2024-11-12 15:45:36 -0500

    Update model.py
    updating to calculate the union mask

--- haca3/modules/model.py
+++ haca3/modules/model.py
@@ -645,8 +645,11 @@ class HACA3:
                     logit_fusion_tmp, attention_tmp = self.attention_module(query_tmp, k, v, masks_tmp, None, 5.0)
                     beta_fusion_tmp = self.channel_aggregation(reparameterize_logit(logit_fusion_tmp))
                     combined_map = torch.cat([beta_fusion_tmp, theta_target.repeat(batch_size, 1, 224, 224)], dim=1)
-                    rec_image_tmp = self.decoder(combined_map) #* masks_tmp[0]
-
+                    masks_cpu = [mask.cpu().numpy() for mask in masks_tmp]
+                    union_mask = np.logical_or.reduce(masks_cpu)
+                    union_mask = torch.from_numpy(union_mask).to(masks_tmp[0].device)
+                    rec_image_tmp = self.decoder(combined_map) * union_mask
+                    
                     rec_image.append(rec_image_tmp)
                     beta_fusion.append(beta_fusion_tmp)
                     logit_fusion.append(logit_fusion_tmp)
